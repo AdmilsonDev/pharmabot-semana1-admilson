d4c143042e6330ab914fdcd8334b6663c15f0a71
admilsonbcosta <admilson.b.costa@academico.unirv.edu.br>
2026-08-07 15:09:26 -0300

    feat: semana_2 / A2. contarPorStatus

--- semana_2/parteA.ts
+++ semana_2/parteA.ts
@@ -1,4 +1,4 @@
-
+// A1. somarCarrinho
 interface Iten {
     preco: number,
     quantidade: number
@@ -18,6 +18,33 @@ function somarCarrinho(itens: { preco: number; quantidade: number }[]): number {
     return 0.0
 }
 
+// somarCarrinho(arrayDeCompras)
+// console.log(valorFinal.toFixed(1))
 
-somarCarrinho(arrayDeCompras)
-console.log(valorFinal.toFixed(1))
+
+// A2. contarPorStatus
+interface Fila {
+    tickets: string[],
+    status: string
+}
+
+const listaDeTickets: Fila[] = [
+    {tickets: [], status: "aberto"},
+    { tickets: ["aberto", "fechado", "aberto", "pendente"], status: "aberto"},
+    { tickets: ["aberto", "fechado"], status: "cancelado"},
+    { tickets: ["Aberto", "aberto"], status: "aberto"},
+] 
+
+function contarPorStatus(fila: Fila): number {
+
+    const statusAbertos = fila.tickets.filter(ticket => 
+        ticket.includes(fila.status) 
+    )
+    return statusAbertos.length;
+}
+
+for (const lista of listaDeTickets) {
+    console.log(contarPorStatus(lista))
+}
+
+
