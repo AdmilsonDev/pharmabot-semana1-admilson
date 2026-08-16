97f43fa0195f2be6d45ee0e9586783315317ec8c
admilsonbcosta <admilson.b.costa@academico.unirv.edu.br>
2026-08-16 09:23:33 -0300

    feat: semana_3 / A2. ticketsAbertos

--- semana_3/parteA.ts
+++ semana_3/parteA.ts
@@ -7,10 +7,21 @@ interface Contatos {
 const listaDeContatos: Contatos[] = [{ nome: "Ana", telefone: "11999990000" }, { nome: "Bruno", telefone: "11988880000" }]
 
 function nomesDosContatos(contatos: Contatos[]): string[] {
-
-    const nomesList = contatos.map((contatos) => contatos.nome)
-
-    return nomesList
+    return contatos.map((contatos) => contatos.nome)
 }
 
 console.log(nomesDosContatos(listaDeContatos))
+
+//A2. ticketsAbertos
+interface Ticket {
+    id: number; 
+    status: string 
+}
+
+const ticketList: Ticket[] = [{ id: 1, status: "aberto" }, { id: 2, status: "fechado" }, { id: 3, status: "aberto" }]
+
+function ticketsAbertos(tickets: Ticket[]): Ticket[] {
+    return ticketList.filter((ticket) => ticket.status === "aberto")
+}
+
+console.log(ticketsAbertos(ticketList))
